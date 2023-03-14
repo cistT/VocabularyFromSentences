49517f7b52ca3e83cfe3a415db0fb6cc71858eef
cistT <cist.b220.t.chitose@gmail.com>
2023-03-14 20:53:23 +0900

    sass(scss)をstorybookで適用できるようにした

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,32 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            modules: {
+              auto: true,
+            },
+            url: false,
+          },
+        },
+        "sass-loader",
+      ],
+      include: path.resolve(__dirname, "../src/"),
+    });
+    return config;
+  },
+};
