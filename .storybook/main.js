9d0650dbd788927071c7a16d7ab02e26a876dfa0
cistT <cist.b220.t.chitose@gmail.com>
2023-03-18 20:14:07 +0900

    storybookファイル内でインポートされている@〜を読み込めるように変更

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,32 +1,36 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const path = require("path");
+const toPath = (_path) => path.join(process.cwd(), _path);
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "storybook-addon-next",
   ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
   },
+  typescript: { reactDocgen: false },
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
-      use: [
-        "style-loader",
-        {
-          loader: "css-loader",
-          options: {
-            modules: {
-              auto: true,
-            },
-            url: false,
-          },
-        },
-        "sass-loader",
-      ],
-      include: path.resolve(__dirname, "../src/"),
+      use: ["style-loader", "css-loader", "sass-loader"],
     });
+    config.resolve.modules = [
+      ...(config.resolve.modules || []),
+      path.resolve(__dirname, "../src"),
+    ];
+
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, "../src"),
+      }),
+    ];
+
     return config;
   },
 };
