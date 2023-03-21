041a3c4208ca1df1b246f6eabfbeb1b416e21bff
cistT <cist.b220.t.chitose@gmail.com>
2023-03-21 15:21:39 +0900

    Prettierで整形

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,17 @@
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-const path = require("path");
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require('path');
 const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  framework: "@storybook/react",
+  framework: '@storybook/react',
   core: {
-    builder: "@storybook/builder-webpack5",
+    builder: '@storybook/builder-webpack5',
   },
   features: {
     emotionAlias: true,
@@ -20,19 +20,19 @@ module.exports = {
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
+      use: ['style-loader', 'css-loader', 'sass-loader'],
     });
     config.resolve.plugins = [
       new TsconfigPathsPlugin({
-        configFile: path.resolve(__dirname, "../src"),
+        configFile: path.resolve(__dirname, '../src'),
       }),
     ];
     config.resolve.alias = {
       ...config.resolve.alias,
-      src: path.resolve(__dirname, "../src"),
-      "@emotion/core": toPath("node_modules/@emotion/react"),
-      "@emotion/styled": toPath("node_modules/@emotion/styled"),
-      "emotion-theming": toPath("node_modules/@emotion/react"),
+      src: path.resolve(__dirname, '../src'),
+      '@emotion/core': toPath('node_modules/@emotion/react'),
+      '@emotion/styled': toPath('node_modules/@emotion/styled'),
+      'emotion-theming': toPath('node_modules/@emotion/react'),
     };
     return config;
   },
