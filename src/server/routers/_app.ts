645a5c36352108b01b45e9d70bde450886878d7a
cistT <cist.b220.t.chitose@gmail.com>
2023-04-16 21:24:04 +0900

    DeepL APIを用いて英文を単語に変換するメソッドを追加

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,16 +1,30 @@
+import { UUID } from 'uuidjs';
 import { z } from 'zod';
 import { procedure, router } from '../trpc';
+import { translator } from '@/lib/deepl';
 
 export const appRouter = router({
-  hello: procedure
+  translate: procedure
     .input(
       z.object({
-        text: z.string(),
+        wordList: z.array(z.string()),
       })
     )
-    .query(({ input }) => ({
-      greeting: `hello ${input.text}`,
-    })),
+    .mutation(async ({ input }) => {
+      const translatedWordList = await translator.translateText(
+        input.wordList,
+        'en',
+        'ja'
+      );
+
+      const parsedTranslateWordList = translatedWordList.map((word, i) => ({
+        id: UUID.generate(),
+        word: input.wordList[i],
+        meaning: word.text,
+        explanation: '',
+      }));
+      return parsedTranslateWordList;
+    }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
