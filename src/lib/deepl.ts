7d5072f95ab6534ab48a4b3a345f56e3a56e23e0
cistT <cist.b220.t.chitose@gmail.com>
2023-04-16 21:13:21 +0900

    ESlintを実行

--- src/lib/deepl.ts
+++ src/lib/deepl.ts
@@ -1,4 +1,4 @@
-import * as deepl from "deepl-node";
+import * as deepl from 'deepl-node';
 
 const authKey = process.env.NEXT_PUBLIC_DEEPL_API_KEY as string;
 export const translator = new deepl.Translator(authKey);
