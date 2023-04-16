10e85409963f842aa3a14d9922ab166ec5c71a8b
cistT <cist.b220.t.chitose@gmail.com>
2023-04-16 21:11:59 +0900

    ブラウザバックをするHooks

---
+++ src/hooks/browserBack.ts
@@ -0,0 +1,10 @@
+import { useRouter } from 'next/router';
+
+const useBrowserBack = () => {
+  const router = useRouter();
+  const browserBack = () => router.back();
+
+  return { browserBack };
+};
+
+export default useBrowserBack;
