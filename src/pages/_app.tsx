e3a2cd2eab0f9e232240f589a03bdebb1ad0e11e
cistT <cist.b220.t.chitose@gmail.com>
2023-04-16 21:21:59 +0900

    レイアウトと単語を各ページに供給するProviderを設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app';
+import Layout from '@/components/Layout/Layout';
+import WordProvider from '@/features/word/components/WordProvider/WordProvider';
 import { trpc } from '@/utils/trpc';
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <WordProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </WordProvider>
 );
 
 export default trpc.withTRPC(App);
