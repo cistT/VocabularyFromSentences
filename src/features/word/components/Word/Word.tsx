6d37c4e90c499f5484a87db86663a63760c9ed08
cistT <cist.b220.t.chitose@gmail.com>
2023-04-17 09:08:47 +0900

    英単語を再生するボタンを追加

--- src/features/word/components/Word/Word.tsx
+++ src/features/word/components/Word/Word.tsx
@@ -1,7 +1,9 @@
-import { TextField, Typography } from '@mui/material';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import { Button, TextField, Typography } from '@mui/material';
 import React, { FC } from 'react';
 import { z } from 'zod';
 import { WordSchema } from '../../types/word.d';
+import speechWord from '../../utils/speechWord';
 import styles from './Word.module.scss';
 
 const WordPropsSchema = WordSchema.omit({ id: true });
@@ -10,11 +12,28 @@ type WordProps = z.infer<typeof WordPropsSchema>;
 
 const Word: FC<WordProps> = (props) => {
   const { word, meaning, explanation } = props;
+  const { startSpeech } = speechWord();
+
+  const onClick = () => {
+    if ('speechSynthesis' in window) {
+      startSpeech(word);
+    } else {
+      // eslint-disable-next-line no-alert
+      alert('このブラウザは音声合成に対応していません。');
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Typography variant='h2' component='h2' className={styles.word}>
         {word}
       </Typography>
+
+      <Button onClick={onClick} className={styles['speech-button']}>
+        <PlayCircleOutlineIcon className={styles.icon} />
+        <Typography component='div'>再生</Typography>
+      </Button>
+
       <form className={styles.form}>
         <TextField label='意味' multiline rows={2} defaultValue={meaning} />
         <TextField label='説明' multiline rows={4} defaultValue={explanation} />
